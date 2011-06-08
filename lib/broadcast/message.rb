6bc2f9cf248b365f87c63be340814541d202e2b9
Marcin Bunsch <marcin@applicake.com>
2011-06-08 19:01:53 +0200

    Made the options Hashie::Mash a trait of Broadcast::Message. When Broadcast::Publishable is included to a class, we should assume that the class has it's own source of message body options.

--- lib/broadcast/message.rb
+++ lib/broadcast/message.rb
@@ -1,3 +1,11 @@
 class Broadcast::Message
   include Broadcast::Publishable
+
+  # Make the options Hashie::Mash a specific trait of Broadcast::Message
+  attr_accessor :options
+
+  def initialize(options = {})
+    @options = Hashie::Mash.new(options)
+  end
+
 end
